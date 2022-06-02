7628d41ba75b2407205c9f000240b6bd25f02917
cozyflexing <cozyflexing@gmail.com>
2022-06-02 20:50:36 +0200

    login functionality

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     # auth
     path("signup/", views.signupuser, name="signupuser"),
     path("logout/", views.logoutuser, name="logoutuser"),
+    path("login/", views.loginuser, name="loginuser"),
     # todo
     path("", views.home, name="home"),
 ]
